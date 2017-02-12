4f5af6cdaf3f762ec52218faef66cde14907fec4
gus <gus@debian>
2017-02-12 12:13:52 -0200

    arrumando o header

--- header.php
+++ header.php
@@ -132,7 +132,7 @@
                           'container'         => 'div',
 //                'container_class'   => 'collapse navbar-collapse',
                           'container_id'      => 'bs-example-navbar-collapse-1',
-                          'menu_class'        => 'nav navbar-nav navbar-right',
+                          'menu_class'        => 'nav navbar-nav navbar-right header-novo',
                           'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                           'walker'            => new wp_bootstrap_navwalker())
                         );
