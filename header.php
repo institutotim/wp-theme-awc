44b31b62c4d4ad4119b897b1afa1abf74b1f523c
gus <gus@debian>
2017-02-13 01:22:19 -0200

    alterado o header

--- header.php
+++ header.php
@@ -60,11 +60,10 @@
     <script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
-
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/institutotim.css">
 
 
-        <div class="section navbar-overtop-tim">
+    <div class="section navbar-overtop-tim">
       <div class="container ">
         <div class="row">
           <div class="col-lg-12">
